a48e64bd69e0ad2caa6f231867953fe63fea72a6
Rik de Wit <rikdewit1997@gmail.com>
2021-02-16 17:41:08 +0100

    cleanup

--- public/script.js
+++ public/script.js
@@ -1,77 +1,76 @@
-(function () {
-
-    /**
-     * Obtains parameters from the hash of the URL
-     * @return Object
-     */
-    function getHashParams() {
-        var hashParams = {};
-        var e, r = /([^&;=]+)=?([^&;]*)/g,
-            q = window.location.hash.substring(1);
-        while (e = r.exec(q)) {
-            hashParams[e[1]] = decodeURIComponent(e[2]);
-        }
-        return hashParams;
+/**
+  * Obtains parameters from the hash of the URL
+  * @return Object
+  */
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
     }
+    return hashParams;
+}
 
-    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-        userProfileTemplate = Handlebars.compile(userProfileSource),
-        userProfilePlaceholder = document.getElementById('user-profile');
+var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+    userProfileTemplate = Handlebars.compile(userProfileSource),
+    userProfilePlaceholder = document.getElementById('user-profile');
 
-    var oauthSource = document.getElementById('oauth-template').innerHTML,
-        oauthTemplate = Handlebars.compile(oauthSource),
-        oauthPlaceholder = document.getElementById('oauth');
+var oauthSource = document.getElementById('oauth-template').innerHTML,
+    oauthTemplate = Handlebars.compile(oauthSource),
+    oauthPlaceholder = document.getElementById('oauth');
 
-    var params = getHashParams();
+var params = getHashParams();
 
-    var access_token = params.access_token,
-        refresh_token = params.refresh_token,
-        error = params.error;
+var access_token = params.access_token,
+    refresh_token = params.refresh_token,
+    error = params.error;
 
-    if (error) {
-        alert('There was an error during the authentication');
+if (error) {
+    alert('There was an error during the authentication');
+} else {
+    if (access_token) {
+        // render oauth info
+        oauthPlaceholder.innerHTML = oauthTemplate({
+            access_token: access_token,
+            refresh_token: refresh_token
+        });
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            },
+            success: function (response) {
+                userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+                console.log(response)
+                $('#login').hide();
+                $('#loggedin').show();
+            }
+        });
     } else {
-        if (access_token) {
-            // render oauth info
-            oauthPlaceholder.innerHTML = oauthTemplate({
-                access_token: access_token,
-                refresh_token: refresh_token
-            });
-
-            $.ajax({
-                url: 'https://api.spotify.com/v1/me',
-                headers: {
-                    'Authorization': 'Bearer ' + access_token
-                },
-                success: function (response) {
-                    userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-                    console.log(response)
-                    $('#login').hide();
-                    $('#loggedin').show();
-                }
-            });
-        } else {
-            // render initial screen
-            $('#login').show();
-            $('#loggedin').hide();
-        }
-
-        document.getElementById('obtain-new-token').addEventListener('click', function () {
-            $.ajax({
-                url: '/refresh_token',
-                data: {
-                    'refresh_token': refresh_token
-                }
-            }).done(function (data) {
-                access_token = data.access_token;
-                oauthPlaceholder.innerHTML = oauthTemplate({
-                    access_token: access_token,
-                    refresh_token: refresh_token
-                });
-            });
-        }, false);
+        // render initial screen
+        $('#login').show();
+        $('#loggedin').hide();
     }
+}
 
+function refreshToken() {
+    $.ajax({
+        url: '/refresh_token',
+        data: {
+            'refresh_token': refresh_token
+        }
+    }).done(function (data) {
+        access_token = data.access_token;
+        oauthPlaceholder.innerHTML = oauthTemplate({
+            access_token: access_token,
+            refresh_token: refresh_token
+        });
+    });
+}
+
+function getPlaylists() {
     $.ajax({
         url: 'https://api.spotify.com/v1/me/playlists',
         headers: {
@@ -81,77 +80,12 @@
             console.log(response.items[1]);
         }
     });
-
-    document.querySelector('#startPlayer').addEventListener('click', () => {
-        startPlayer();
-    });
-
-
-
-    window.onSpotifyWebPlaybackSDKReady = () => {
-        isPlayerReady = true;
-        startPlayer();
-    };
-
-    const startPlayer = () => {
-        if (!isPlayerReady) return;
-
-        player = new Spotify.Player({
-            name: 'Spotify Quiz',
-            getOAuthToken: cb => { cb(access_token); },
-            volume: 0.5
-        });
-
-        // Error handling
-        player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        player.addListener('account_error', ({ message }) => { console.error(message); });
-        player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-        // Playback status updates
-        player.addListener('player_state_changed', state => { console.log(state); });
-
-        // Ready
-        player.addListener('ready', ({ device_id }) => {
-            console.log('Ready with Device ID', device_id);
-            // Start playing!
-            play({
-                playerInstance: player,
-                spotify_uri: 'spotify:track:5V2oQWEqjATwMtMo26JXDM',
-            });
-        });
-
-        // Not Ready
-        player.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id);
-        });
-
-        // Connect to the player!
-        player.connect();
-
-    };
-
-    const play = ({
-        spotify_uri,
-        playerInstance,
-    }) => {
-        playerInstance._options.getOAuthToken(access_token => {
-            fetch(`https://api.spotify.com/v1/me/player/play?device_id=${playerInstance._options.id}`, {
-                method: 'PUT',
-                body: JSON.stringify({ uris: [spotify_uri] }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${access_token}`
-                },
-            })
-                .then(response => {
-                    console.log(response)
-                });
-        });
-    };
+}
 
 
 
 
 
-})();
+
+
+
