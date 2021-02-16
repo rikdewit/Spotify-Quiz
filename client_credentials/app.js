10f2e695ad0797f8cd7cae36b0bdd7f044c266e0
Rik de Wit <rikdewit1997@gmail.com>
2021-02-16 15:54:04 +0100

    spotify playback

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -9,8 +9,8 @@
 
 var request = require('request'); // "Request" library
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
+var client_id = '702808c658124e418363ffd1020770b5'; // Your client id
+var client_secret = '6e1975354b374e36b104f84444bd074f'; // Your secret
 
 // your application requests authorization
 var authOptions = {
@@ -24,7 +24,7 @@ var authOptions = {
   json: true
 };
 
-request.post(authOptions, function(error, response, body) {
+request.post(authOptions, function (error, response, body) {
   if (!error && response.statusCode === 200) {
 
     // use the access token to access the Spotify Web API
@@ -36,7 +36,7 @@ request.post(authOptions, function(error, response, body) {
       },
       json: true
     };
-    request.get(options, function(error, response, body) {
+    request.get(options, function (error, response, body) {
       console.log(body);
     });
   }
