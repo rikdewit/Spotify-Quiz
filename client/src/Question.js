d8ac0a7f651a6ae724dd787e3fe758b60d4d3a09
Rik de Wit <rikdewit1997@gmail.com>
2021-02-17 18:17:56 +0100

    added timer

--- client/src/Question.js
+++ client/src/Question.js
@@ -5,6 +5,8 @@ export function Question(props) {
     const [started, setStarted] = useState(false);
     const [playing, setPlaying] = useState(false);
     const [audio, setAudio] = useState();
+    const [progress, setProgress] = useState();
+
     useEffect(() => {
         if (audio) {
             pause();
@@ -12,18 +14,40 @@ export function Question(props) {
         }
     }, [props.number])
 
+
+
+
+    useEffect(() => {
+        console.log("next")
+        let interval;
+        let timer = 30;
+        setProgress(timer);
+
+        interval = setInterval(() => {
+            if (timer > 0 && started) {
+                timer--;
+                setProgress(timer);
+            }
+        }, 1000);
+
+        return () => { clearInterval(interval); };
+
+    }, [props.number, started]);
+
+
+
+
     function loadAudio(play) {
         setPlaying(false);
 
         let a = new Audio(props.track.preview_url);
-        setAudio(a);
         a.currentTime = 0;
+        a.volume = 0.2;
         if (play) {
             a.play();
             setPlaying(true);
         }
-
-
+        setAudio(a);
     }
 
     function pause() {
@@ -46,10 +70,12 @@ export function Question(props) {
                 <div>
                     <h2>{props.track.name}</h2>
                     <h3>{props.number}</h3>
+                    <h3>Time: {progress}</h3>
                     <button onClick={props.newQuestion}> Next Question</button>
+                    <button onClick={pause}>stop</button>
                 </div>
                 :
-                <button onClick={() => { setStarted(true); loadAudio(true) }}>Start</button>
+                <button onClick={() => { setStarted(true); loadAudio(true); }}>Start</button>
             }
 
         </div>
