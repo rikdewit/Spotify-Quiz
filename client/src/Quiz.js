542f91bdf65be59a6c5b81ba6651ca9b80a77474
Rik de Wit <rikdewit1997@gmail.com>
2021-02-18 21:13:21 +0100

    fixed audio on mobile

--- client/src/Quiz.js
+++ client/src/Quiz.js
@@ -13,12 +13,21 @@ export function Quiz(props) {
     const [end, setEnd] = useState(false);
     const [started, setStarted] = useState(false);
     const [muted, setMuted] = useState(true);
-
+    const [audios, setAudios] = useState([]);
 
     useEffect(() => {
         getTrackInfoTop2000(accessToken, user.country).then((trackInfo) => {
             setTracks(trackInfo);
             setTrack(trackInfo[questionN]);
+
+            let sounds = [];
+            for (const t of trackInfo) {
+                let s = new Audio(t.preview_url);
+                s.volume = 0.2;
+                sounds.push(s);
+            }
+
+            setAudios(sounds);
         })
 
     }, [user, accessToken]);
@@ -27,6 +36,8 @@ export function Quiz(props) {
         if (questionN + 1 < tracks.length) {
             setTrack(tracks[questionN + 1]);
             setQuestionN(questionN + 1);
+            play(questionN + 1)
+
         } else {
             setEnd(true);
         }
@@ -40,10 +51,24 @@ export function Quiz(props) {
         setMuted(false);
     }
 
+    function play(n) {
+        for (const audio of audios) {
+            audio.pause();
+        }
+        if (n) {
+            audios[n].play();
+        } else {
+            audios[questionN].play();
+        }
+    }
+
     function start() {
         setStarted(true);
         setQuestionN(0);
+        play();
     }
+
+
 
 
     return (
@@ -54,7 +79,7 @@ export function Quiz(props) {
             <br></br>
 
             {track ? <Question end={end} number={questionN} totalNumber={tracks.length} track={track} newQuestion={newQuestion} start={start} mute={mute} unMute={unMute} muted={muted} /> : "No track"}
-            <AudioPlayer sources={tracks} playing={questionN} started={started} muted={muted} />
+            {/* <AudioPlayer sources={tracks} playing={questionN} started={started} muted={muted} /> */}
         </div>
 
     );
