9873a870392791e7a7fc06f614e37d32438c6175
Rik de Wit <rikdewit1997@gmail.com>
2021-02-19 15:22:25 +0100

    style

--- client/src/Question.js
+++ client/src/Question.js
@@ -72,41 +72,48 @@ export function Question(props) {
                 <div>
                     {started ?
                         <div>
-                            <h1>Guess the release year of the song</h1>
-                            <br />
-                            {progress == 0 ?
-                                <div>
-                                    <h2>{props.track.name}</h2>
-                                    <h2>{props.track.artist}</h2>
-                                    <h2>{props.track.release_year}</h2>
-                                </div>
-                                :
-                                <div>
-                                    <h2>?</h2>
-                                    <h2>?</h2>
-                                    <h2>?</h2>
+                            <div className="card">
 
-                                </div>
-                            }
-                            < br />
-                            <h3>Song {props.number + 1}/{props.totalNumber}</h3>
-                            <h3>Time: {progress}</h3>
-                            {/* <ProgressBar progress={progress / timeAllowed} /> */}
-                            <LinearProgress
-                                color="primary"
-                                variant="determinate"
-                                value={100 * progress / timeAllowed}
-                            />
+                                {progress == 0 ? <div><h2>You got {currentPoints} points!</h2> <h2>Your score: {score}</h2></div> : <h2>Your score: {score - currentPoints}</h2>}
 
-                            <QuestionInput change={handleChange} val={inputValue} submit={handleSubmit} />
-                            <button className="Spotify" onClick={() => { props.newQuestion(); setCurrentPoints(0); setGuess(null) }}> Next Song</button>
-                            <button className="Spotify" onClick={props.muted ? props.unMute : props.mute}>{props.muted ? "unmute" : "mute"}</button>
-                            {guess ? <h2>You guessed {guess}</h2> : ""}
-                            {progress == 0 ? <div><h2>You got {currentPoints} points!</h2> <h2>Your score: {score}</h2></div> : <h2>Your score: {score - currentPoints}</h2>}
+                                <br />
+                                {progress == 0 ?
+                                    <div>
+                                        <h2>{props.track.name}</h2>
+                                        <h2>{props.track.artist}</h2>
+                                        <h2>{props.track.release_year}</h2>
+                                    </div>
+                                    :
+                                    <div>
+                                        <h2>?</h2>
+                                        <h2>?</h2>
+                                        <h2>?</h2>
 
+                                    </div>
+                                }
+                                < br />
+                                <h3>Song {props.number + 1}/{props.totalNumber}</h3>
+                                <h3>Time: {progress}</h3>
+                                {/* <ProgressBar progress={progress / timeAllowed} /> */}
+                                <LinearProgress
+                                    color="primary"
+                                    variant="determinate"
+                                    value={100 * progress / timeAllowed}
+                                />
+
+                                <QuestionInput change={handleChange} val={inputValue} submit={handleSubmit} />
+                                <button className="Spotify" onClick={() => { props.newQuestion(); setCurrentPoints(0); setGuess(null) }}> Next Song</button>
+                                <button className="Spotify" onClick={props.muted ? props.unMute : props.mute}>{props.muted ? "unmute" : "mute"}</button>
+                                {guess ? <h2>You guessed {guess}</h2> : ""}
+                                {progress == 0 ? <div><h2>You got {currentPoints} points!</h2> <h2>Your score: {score}</h2></div> : <h2>Your score: {score - currentPoints}</h2>}
+                            </div>
                         </div>
                         :
-                        <button className="Spotify" onClick={() => { props.start(); setStarted(true); }}>Start</button>
+                        <div>
+                            <h1>Guess the release year of the song</h1>
+
+                            <button className="Spotify" onClick={() => { props.start(); setStarted(true); }}>Start</button>
+                        </div>
                     }
                 </div>
                 : <div>
