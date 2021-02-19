d9af2378e9f687779f3700beef9728b8270076cc
Rik de Wit <rikdewit1997@gmail.com>
2021-02-19 14:43:14 +0100

    styles

--- client/src/QuestionInput.js
+++ client/src/QuestionInput.js
@@ -6,19 +6,20 @@ export default function QuestionInput(props) {
 
     return (
         <div>
-            <DatePicker
-                color="primary"
-                label="year"
-                views={["year"]}
-                value={props.val}
-                onChange={props.change}
-                minDate={new Date("1910-01-01")}
-                maxDate={new Date()}
-                variant="dialog"
-                autoOk={true}
-                className="Picker"
-                fullWidth={true}
-            />
+            <div className="Picker">
+                <DatePicker
+                    color="primary"
+                    label="year"
+                    views={["year"]}
+                    value={props.val}
+                    onChange={props.change}
+                    minDate={new Date("1910-01-01")}
+                    maxDate={new Date()}
+                    variant="dialog"
+                    autoOk={true}
+                    fullWidth={true}
+                />
+            </div>
             <br></br>
 
             <button className="Spotify" onClick={props.submit}>Submit</button>
