aef1acdcae909534d43dc58bece383f287422a02
Rik de Wit <rikdewit1997@gmail.com>
2021-02-20 20:40:52 +0100

    fixed title

--- client/src/LoginButton.js
+++ client/src/LoginButton.js
@@ -1,10 +1,15 @@
 
 export function LoginButton() {
     return (
-        <div className="card flexCenterColumn">
-            <h1>Spotify Quiz</h1>
-            <br></br>
-            <a href={process.env.REACT_APP_BACKEND + "/login"} className="Spotify">Log in</a>
+        <div>
+            <div className="card flexCenterRow TopBar">
+                <img className="SpotifyLogo" src="/img/Spotify_Icon_RGB_Green.png"></img>
+                <h1 className="TopTitle" >SpotifyQuiz</h1>
+
+            </div>
+            <div className="card flexCenterColumn">
+                <a href={process.env.REACT_APP_BACKEND + "/login"} className="Spotify">Log in</a>
+            </div>
         </div>
     );
 }
