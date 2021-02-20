fb87cce92a017a705e2903bcc30bcb1b278f0c8e
Rik de Wit <rikdewit1997@gmail.com>
2021-02-20 19:05:56 +0100

    edit coffee color

--- client/src/BuyCoffee.js
+++ client/src/BuyCoffee.js
@@ -2,7 +2,7 @@ export default function BuyCoffee() {
 
     return (
         <a href="https://www.buymeacoffee.com/rikdewit">
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=rikdewit&button_colour=1db954&font_colour=ffffff&font_family=Lato&outline_colour=191414&coffee_colour=8B5D33">
+            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=rikdewit&button_colour=1db954&font_colour=ffffff&font_family=Lato&outline_colour=191414&coffee_colour=f0e013">
             </img>
         </a>
 
