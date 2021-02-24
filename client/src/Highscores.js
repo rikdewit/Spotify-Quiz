3ef0eb9a41df0cdf7b68cb79a003fe410bd9223e
Rik de Wit <rikdewit1997@gmail.com>
2021-02-24 01:47:22 +0100

    added highscores list

---
+++ client/src/Highscores.js
@@ -0,0 +1,33 @@
+import firestore from './services/firebase';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useEffect } from 'react';
+import { timeAgo } from './services/utils'
+import Table from '@material-ui/core/Table'
+
+
+export default function Highscores(props) {
+    const highScoresRef = firestore.collection('Highscores');
+    const query = highScoresRef.orderBy('score', 'desc').limit(10);
+    const [scores] = useCollectionData(query, { idField: 'id' });
+
+
+
+    return (
+        <div className="HighscoresContainer">
+            <h1>Highscores</h1>
+            <table className="HighscoresTable">
+                <colgroup>
+                    <col width="4%" />
+                    <col width="70%" />
+                    <col width="6%" />
+                    <col width="20%" />
+                </colgroup>
+                <tbody>
+                    {scores && scores.map((score, index) => <tr key={score.id}><td>{index + 1}</td><td className="NameRow">{score.name}</td> <td>{score.score}</td><td className="TimeStamp">{score.timeStamp ? timeAgo(score.timeStamp.seconds) : null}</td></tr>)}
+                </tbody>
+            </table>
+
+            <Table />
+        </div>
+    )
+}
