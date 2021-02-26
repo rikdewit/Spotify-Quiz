3994b0c09b1e1b7ba595f637342cc8cdf6fa2c07
Rik de Wit <rikdewit1997@gmail.com>
2021-02-26 01:23:56 +0100

    coffeecount updates every day

--- client/src/BuyCoffee.js
+++ client/src/BuyCoffee.js
@@ -1,10 +1,14 @@
 export default function BuyCoffee() {
+    function getDate() {
+        let date = new Date().getDate();
+        return date
+    }
 
     return (
         <a href="https://www.buymeacoffee.com/rikdewit" className="BuyCoffee">
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=rikdewit&button_colour=1db954&font_colour=ffffff&font_family=Lato&outline_colour=191414&coffee_colour=f0e013">
+            <img src={"https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=rikdewit&button_colour=1db954&font_colour=ffffff&font_family=Lato&outline_colour=191414&coffee_colour=f0e013&" + getDate()}>
             </img>
-        </a>
+        </a >
 
     )
 }
