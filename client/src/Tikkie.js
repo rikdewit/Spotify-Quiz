3877bac949cdc4a33991ed148a9a66c805f7c152
Rik de Wit <rikdewit1997@gmail.com>
2021-03-22 14:33:55 +0100

    Update tikkie link

--- client/src/Tikkie.js
+++ client/src/Tikkie.js
@@ -2,7 +2,7 @@
 
 export default function Tikkie(props) {
     return (
-        <a className="Tikkie" href="https://tikkie.me/pay/c6ckvc34m089pucrebf9">
+        <a className="Tikkie" href="https://tikkie.me/pay/odv231ibqb4k4plokacj">
             <img src="/img/tikkie.svg"></img>
         </a>
     )
