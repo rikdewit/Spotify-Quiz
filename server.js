c6f6dd276bdc75bb57eb8c92cd8cc9767ce401dc
Rik de Wit <rikdewit1997@gmail.com>
2021-06-22 23:18:12 +0200

    Change permissions to spotify api: removed streaming

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/login', function (req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email streaming user-read-email user-read-private';
+  var scope = 'user-read-private user-read-email';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
