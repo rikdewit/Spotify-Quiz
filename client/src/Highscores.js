df12a86503a754887d910f2498c091c7cd7a4ae6
Rik de Wit <rikdewit1997@gmail.com>
2021-12-18 01:01:34 +0100

    display user badges

--- client/src/Highscores.js
+++ client/src/Highscores.js
@@ -1,6 +1,7 @@
 import firestore from './services/firebase';
+import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { timeAgo } from './services/utils'
 import Table from '@material-ui/core/Table'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,7 +12,50 @@ import { Badges } from './Badges';
 export default function Highscores(props) {
     const highScoresRef = firestore.collection('Highscores');
     const query = highScoresRef.orderBy('score', 'desc').limit(10);
-    const [scores] = useCollectionData(query, { idField: 'id' });
+    // const [scores] = useCollectionData(query, { idField: 'id' });
+    const [users, setUsers] = useState();
+    const [scores, setScores] = useState();
+
+
+    useEffect(() => {
+        getUserData();
+        async function getUserData() {
+            const scoresFS = await query.get();
+            let scoreArray = [];
+            scoresFS.forEach((doc) => {
+                let data = doc.data();
+                data['userId'] = doc.id;
+                scoreArray.push(data);
+            })
+            setScores(scoreArray);
+            console.log(scoreArray);
+
+            if (scoreArray) {
+                let ids = scoreArray.map((score) => {
+                    return score.userId
+                })
+
+                const usersRef = firestore.collection("Users");
+                let queryUsers = await usersRef.where(firebase.firestore.FieldPath.documentId(), "in", ids).get();
+                let usersArray = [];
+                queryUsers.forEach((doc) => {
+                    usersArray.push(doc.data());
+                });
+                setUsers(usersArray);
+            }
+
+        }
+    }, []);
+
+    function getUserBadges(index) {
+        let badges = [];
+        try {
+            badges = users[index].badges
+        } catch {
+            console.log("no user for that index")
+        }
+        return badges
+    }
 
 
 
@@ -28,10 +72,10 @@ export default function Highscores(props) {
                 </colgroup>
                 <tbody>
                     {scores && scores.map((score, index) =>
-                        <tr key={score.id}>
+                        <tr key={score.userId}>
                             <td>{index + 1} </td>
                             <td className="NameRow">
-                                <Badges badges={score.badges} />
+                                <Badges badges={getUserBadges(index)} />
                                 <p className="donatorBadge"> {score.donator ? <FontAwesomeIcon icon={faDollarSign} /> : null} </p>
                                 <p className="scoreName"> {score.name} </p>
                             </td>
